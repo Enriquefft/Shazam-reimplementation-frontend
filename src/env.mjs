7084340d40417887917b321c2af7c51a672ffc8d
Enriquefft <enriquefft2001@gmail.com>
2024-07-11 06:25:13 -0500

    fix: correctend required .env

--- src/env.mjs
+++ src/env.mjs
@@ -3,19 +3,12 @@ import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
 export const env = createEnv({
-  server: {
-    DRIZZLE_DATABASE_URL: z.string().url(),
-
-    AUTH_SECRET: z.string(),
-  },
+  server: {},
   client: {
     NEXT_PUBLIC_PROJECT_NAME: z.string(),
   },
   runtimeEnv: {
     NEXT_PUBLIC_PROJECT_NAME: process.env["NEXT_PUBLIC_PROJECT_NAME"],
-    DRIZZLE_DATABASE_URL: process.env["DRIZZLE_DATABASE_URL"],
-
-    AUTH_SECRET: process.env["AUTH_SECRET"],
   },
   /*
    * For Next.js >= 13.4.4, you only need to destructure client variables:
